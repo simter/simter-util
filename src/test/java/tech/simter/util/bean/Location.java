ede28a0ef6672d91a3c2741d56f3660312ae8461
RJ.HWang <rongjihuang@gmail.com>
2017-08-17 09:21:30 +0800

    Add BeanUtilsTest

---
+++ src/test/java/tech/simter/util/bean/Location.java
@@ -0,0 +1,6 @@
+package tech.simter.util.bean;
+
+public class Location {
+  public Integer id;
+  public String name;
+}
---
+++ src/test/java/tech/simter/util/bean/UserDto.java
@@ -0,0 +1,8 @@
+package tech.simter.util.bean;
+
+public class UserDto {
+  public Integer id;
+  public String account;
+  public Integer locationId;
+  public String locationName;
+}
