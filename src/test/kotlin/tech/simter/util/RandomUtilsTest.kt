52933aeadc3b9c3e7b5592364e75fb97fd14295e
RJ <rongjihuang@gmail.com>
2020-04-15 12:16:18 +0800

    Polishing RandomUtilsTest code

--- src/test/kotlin/tech/simter/util/RandomUtilsTest.kt
+++ src/test/kotlin/tech/simter/util/RandomUtilsTest.kt
@@ -1,11 +1,18 @@
 package tech.simter.util
 
-import org.junit.jupiter.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import tech.simter.util.RandomUtils.nextId
+import tech.simter.util.RandomUtils.nextInt
+import tech.simter.util.RandomUtils.nextLong
+import tech.simter.util.RandomUtils.randomBigDecimal
 import tech.simter.util.RandomUtils.randomBoolean
 import tech.simter.util.RandomUtils.randomInt
+import tech.simter.util.RandomUtils.randomLong
 import tech.simter.util.RandomUtils.randomString
+import java.math.BigDecimal
 
 /**
  * @author RJ
@@ -14,38 +21,99 @@ class RandomUtilsTest {
   @Test
   fun testRandomBoolean() {
     assertNotNull(randomBoolean())
-    //(0..9).forEach { println(randomBoolean()) }
   }
 
   @Test
   fun testRandomString() {
-    assertNotNull(randomString())
-    //(0..9).forEach { println(randomBoolean()) }
+    assertEquals(6, randomString(len = 6).length)
+    assertEquals(36, randomString().length)
+    assertThat(randomString(prefix = "abc")).startsWith("abc")
+    repeat(1000) {
+      assertThat(randomString(prefix = "abc", len = 10)).startsWith("abc").hasSizeLessThanOrEqualTo(10)
+    }
   }
 
   @Test
   fun testRandomInt() {
-    (0..1000).forEach {
-      val i = randomInt()
-      assertTrue(i >= 0)
-      assertTrue(i <= 9)
+    repeat(1000) {
+      assertThat(randomInt())
+        .isGreaterThanOrEqualTo(0)
+        .isLessThanOrEqualTo(100)
     }
-    (0..1000).forEach {
-      val i = randomInt(0, 99)
-      assertTrue(i >= 0)
-      assertTrue(i <= 100)
+    repeat(1000) {
+      assertThat(randomInt(0, 10))
+        .isGreaterThanOrEqualTo(0)
+        .isLessThanOrEqualTo(10)
     }
-    (0..1000).forEach {
-      val i = randomInt(100, 999)
-      assertTrue(i >= 100)
-      assertTrue(i <= 999)
+    repeat(1000) {
+      assertThat(randomInt(100, 110))
+        .isGreaterThanOrEqualTo(100)
+        .isLessThanOrEqualTo(110)
+    }
+  }
+
+  @Test
+  fun testRandomLong() {
+    repeat(1000) {
+      assertThat(randomLong())
+        .isGreaterThanOrEqualTo(0)
+        .isLessThanOrEqualTo(100)
+    }
+    repeat(1000) {
+      assertThat(randomLong(0, 10))
+        .isGreaterThanOrEqualTo(0)
+        .isLessThanOrEqualTo(10)
+    }
+    repeat(1000) {
+      assertThat(randomLong(100, 110))
+        .isGreaterThanOrEqualTo(100)
+        .isLessThanOrEqualTo(110)
+    }
+  }
+
+  @Test
+  fun testRandomBigDecimal() {
+    repeat(1000) {
+      assertThat(randomBigDecimal())
+        .isGreaterThanOrEqualTo(BigDecimal.ZERO)
+        .isLessThanOrEqualTo(BigDecimal("100.00"))
+    }
+    repeat(1000) {
+      assertThat(randomBigDecimal(0, 10))
+        .isGreaterThanOrEqualTo(BigDecimal.ZERO)
+        .isLessThanOrEqualTo(BigDecimal("10.00"))
+    }
+    repeat(1000) {
+      assertThat(randomBigDecimal(100, 110))
+        .isGreaterThanOrEqualTo(BigDecimal("100.00"))
+        .isLessThanOrEqualTo(BigDecimal("110.00"))
+    }
+  }
+
+  @Test
+  fun testNextInt() {
+    var currentInt = 0
+    repeat(1000) {
+      val nextInt = nextInt()
+      assertEquals(currentInt + 1, nextInt)
+      currentInt = nextInt
+    }
+  }
+
+  @Test
+  fun testNextLong() {
+    var currentLong = 0L
+    repeat(1000) {
+      val nextLong = nextLong()
+      assertEquals(currentLong + 1, nextLong)
+      currentLong = nextLong
     }
   }
 
   @Test
   fun testNextId() {
     var currentId = 0
-    (0..1000).forEach {
+    repeat(1000) {
       val nextId = nextId()
       assertEquals(currentId + 1, nextId)
       currentId = nextId
